466c12c4727e597bc2c6cc5f1dc5b194ccf4b7a3
beetleskin <stfn.ka@gmail.com>
2014-08-10 16:59:06 +0200

    gpu hog added

--- untitled.cpp
+++ untitled.cpp
@@ -8,32 +8,17 @@
 using namespace std;
 using namespace cv;
 
-#define PATH_SEP "/"
-
-//18 0 0 1 16 3 0 0 17 
 
 int main(int argc, char const *argv[]) {
-	vector<Mat> hogVector(9);
-	vector<Mat> channels;
-	HoG hog;
+
+
 	Mat img = imread("/home/stfn/dev/rgbd-dataset/rgbd-dataset/cereal_box/cereal_box_1/cereal_box_1_1_1_crop.png");
-	split(img, channels);
-	{
-		boost::progress_timer t;
-		for (int i = 0; i < 100; ++i) {
-			for (int i = 0; i < 9; ++i) {
-				hogVector[i] = Mat::zeros(img.cols, img.rows, CV_8UC1);
-			}
-			hog.extractOBin(channels[0], channels[1], hogVector);
-		}
+	Mat framed_img;
+	copyMakeBorder(img, framed_img, 0, 30, 0, 50, BORDER_REPLICATE);
 
-	}
-	for (int i = 0; i < 9; ++i) {
-		cout << (int)hogVector[i].at<uchar>(10, 10) << " ";
-	}
-	cout << endl;
-
-	return 0;
+	imshow("f", framed_img);
+	imshow("img", img);
+	waitKey();
 
 
 
